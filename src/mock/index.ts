b03064a4518eeb2647ffe3c2813b92edc3e535ba
Ivan <java-zhara@mail.ru>
2023-06-20 12:06:34 +0300

    github-card

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from 'types';
+
+export const defaultUser: LocalGithubUser = {
+  login: 'Java-zhara',
+  avatar: 'https://avatars.githubusercontent.com/u/74813747?v=4',
+  name: 'Ivan',
+  company: 'Clevertec',
+  blog: '',
+  location: 'Belarus',
+  bio: 'Frontend Web Developer',
+  twitter: '',
+  repos: 38,
+  followers: 1,
+  following: 0,
+  created: '2020-11-21T08:43:34Z',
+};
